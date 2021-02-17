a88eaf2b818f22fff2ea7e78c7cadedbbb52f268
shanilpanara <sp2017@ic.ac.uk>
2021-02-17 23:18:22 +0000

    Added simple test with advanced pytest features, more indepth assertions and tests required however

--- test/test_staples/test_algorithm_1.py
+++ test/test_staples/test_algorithm_1.py
@@ -2,44 +2,52 @@ from drawNA.lattice.route import LatticeRoute
 from drawNA.lattice import LatticeRoute
 from drawNA.polygons import BoundaryPolygon
 from drawNA.oxdna import System
-from ...sandbox.staples_from_route import StapleBaseClass, StapleContainer, StaplingAlgorithm1
+from sandbox.staples_from_route import StapleBaseClass, StapleContainer, StaplingAlgorithm1
 
 import numpy as np
+import pytest
 
-def test_run_staple_algorithm_1(domain_size = 15) -> StapleBaseClass:
+# 1. Arrange
+@pytest.fixture
+def scaffold() -> LatticeRoute: 
     # Create Scaffold strand object (i.e. LatticeRoute)
+    polygon_vertices = np.array([[0.,10.,0.],[2.5,4.,0.],[7.5,4.,0.],[10.,10.,0.]])*6 # trapezium
     polygon = BoundaryPolygon(polygon_vertices) # this step can be omitted in theory but shouldn't be
     scaffold = polygon.dna_snake(start_side="left", grid_size = [0.34, 2])
     route = scaffold.route()
+    route.plot()
+    return route
 
+# 2. Act
+@pytest.fixture
+def stapled_scaffold(scaffold: LatticeRoute) -> StapleBaseClass:    
     # Generate staples for all nucleotides on scaffold
-    print("Test: Generating staples for all nucleotides on the scaffold")
-    stapled_scaffold = StaplingAlgorithm1(route, domain_size = domain_size)
+    stapled_scaffold = StaplingAlgorithm1(scaffold, domain_size = 15)
     return stapled_scaffold
 
-def test_create_staple_container(stapled_scaffold: StapleBaseClass) -> StapleContainer:
+# 3. Assert
+def test_container(stapled_scaffold: StapleBaseClass):
     # Generate oxDNA strand objects for every staple and add to a container
-    print("Test: Adding staples to container")
-    strand_container = stapled_scaffold.generate_container()
-    return strand_container
+    container = stapled_scaffold.generate_container()
 
-def test_return_system(container: StapleContainer) -> System:
-    print("Test: Adding staples to an oxDNA system...")
-    return container.system()
-    
-
-
-if __name__ == "__main__":
-    # Create Scaffold strand object (i.e. LatticeRoute)
-    polygon_vertices = np.array([[0.,10.,0.],[2.5,4.,0.],[7.5,4.,0.],[10.,10.,0.]]) # trapezium
-    polygon = BoundaryPolygon(polygon_vertices) # this step can be omitted in theory but shouldn't be
-    scaffold = polygon.dna_snake(start_side="left", grid_size = [0.34, 2])
-    route = scaffold.route()
-
-    # Create Stapled Scaffold Object
-    stapled_scaffold = test_run_staple_algorithm_1(route)
-    # Add all strands into a StapleContainer
-    strand_container = test_create_staple_container(stapled_scaffold)
-    # Return oxDNA system
-    system = test_return_system(strand_container)
+    # Generate oxDNA system filled with strand objects
+    system = container.system()
     system.write_oxDNA(prefix = "test_trapezium")
+
+    # Assertion statements
+    assert container.n_staples == stapled_scaffold.staple_ID - 1
+    ## add lots more later
+
+    return container
+
+
+## Test in jupyter interactive terminal
+# !pytest test_algorithm_1.py
+
+# polygon_vertices = np.array([[0.,10.,0.],[2.5,4.,0.],[7.5,4.,0.],[10.,10.,0.]])*6 # trapezium
+# polygon = BoundaryPolygon(polygon_vertices) # this step can be omitted in theory but shouldn't be
+# scaffold = polygon.dna_snake(start_side="left", grid_size = [0.34, 2])
+# route = scaffold.route()
+# route.plot()
+# stapled_scaf = StaplingAlgorithm1(route, domain_size = 15)
+# container = stapled_scaf.generate_container()
