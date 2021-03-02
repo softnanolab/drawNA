e4b8e74dda25cb224ef6763b2c0ee1e8d62cd78b
Christopher Lau <lauchristopher98@gmail.com>
2021-03-02 20:29:37 +0800

    Added derivatives, did basic testing, further optimization needed, no updated matrix generation yet

--- sandbox/Spring_system.py
+++ sandbox/Spring_system.py
@@ -2,19 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import itertools
-
+ 
 import pandas as pd
 '''
 The Node class creates a node object that has a position accessed by self.get_position. The set_position method allows the user
-to input a numpy array with elements 0,1,2 representing the e0, e1 and e2 vectors. e0 is the position vector of the node, e1
-points towards the major groove, e2 points towards the base in a chosen strand and e3 is automatically calculated by taking the 
+to input a numpy array with elements 0,1,2,3 representing the e0, e1, e2, and e3 vectors, respectively. e0 is the position vector of the node, e1
+points towards the major groove, e2 points towards the base in a chosen strand and e3 is the 
 vector product of e1 and e2
 '''
-
+ 
 class Node:
     id_iter = itertools.count()
     def __init__(self):
-        self.position = np.array([[0,0,0], [0,0,0], [0,0,0], [0,0,0], [0,0,0]])
+        self.position = np.array([[0,0,0], [0,0,0], [0,0,0], [0,0,0]])
         self.index = next(Node.id_iter)
     def set_position(self, e0, e1, e2, e3):
         '''
@@ -29,18 +29,27 @@ class Node:
         
     def get_position(self):
         return self.position
-
+ 
 # test_node = Node()
 # test_node.set_position([1,1,1], [0, -1, -1], [0, -2, -1])
 # print(type(test_node.position)) # returns ndarray
 # print(test_node.position)
-
+ 
 class Connector:
     def __init__(self, node_1, node_2):
         assert isinstance(node_1, Node)
         assert isinstance(node_2, Node)
         self.nodes = node_1, node_2
         self.node_index = [node_1.index, node_2.index]
+        #Calculates unit vectors of node 1 for e1, e2 and e3 vectors
+        self.N1_e1_u = node_1.position[1] / np.linalg.norm(node_1.position[1])
+        self.N1_e2_u = node_1.position[2] / np.linalg.norm(node_1.position[2])
+        self.N1_e3_u = node_1.position[3] / np.linalg.norm(node_1.position[3])
+        #Calculates unit vectors of node 2 for e1, e2 and e3 vectors
+        self.N2_e1_u = node_2.position[1] / np.linalg.norm(node_2.position[1])
+        self.N2_e2_u = node_2.position[2] / np.linalg.norm(node_2.position[2])
+        self.N2_e3_u = node_2.position[3] / np.linalg.norm(node_2.position[3]) 
+ 
     def connect_nodes(self, node_1, node_2):
         self.starting_pos = node_1.position[0]
         self.ending_pos = node_2.position[0]
@@ -58,7 +67,7 @@ class Connector:
         v6_u = unit_vector(node_2.position[3])
         self.bend_angle_2 = np.arccos(np.clip(np.dot(v5_u, v6_u), -1.0, 1.0))
         return self
-
+ 
 class Beam(Connector):
     def __init__(self, *args):
         super().__init__(*args)
@@ -99,15 +108,15 @@ class Spring(Connector):
         self.rot_x = 1353 #pN nm rad-1
         self.rot_y = 1353 #pN nm rad-1
         self.rot_z = 135.3 #pN nm rad-1
-
+ 
         self._bulge = False
-
+ 
         self.bulge = bulge
-
+ 
     @property
     def bulge(self):
         return self._bulge
-
+ 
     @bulge.setter
     def bulge(self, value: bool):
         assert isinstance(value, bool)
@@ -127,7 +136,7 @@ class Spring(Connector):
         rot_z_energy = 0.5 * self.rot_z * self.twist_angle ** 2
         total_energy = rot_x_energy + rot_y_energy + rot_z_energy
         return total_energy
-
+ 
 test_node_1 = Node()
 test_node_1.set_position([0, 0, 0], [0, -1, -1], [0, -2, -1], [1, 1, 1])
 test_node_2 = Node()
@@ -144,8 +153,8 @@ system = [test_node_1, test_node_2, test_node_3, test_beam_4,
           test_beam_1, test_beam_2, test_beam_3, test_node_4
           ]
 test_spring = Spring(test_node_1, test_node_2)
-
-
+ 
+ 
 def visualise_spring_system(system):    
     node_pos = []
     x_pos_node = []
@@ -168,57 +177,165 @@ def visualise_spring_system(system):
     ax.scatter3D(x_pos_node, y_pos_node, z_pos_node, color = "green")
     
     plt.show()
-
+ 
 def generate_matrix(system):
     node_counter = 0 
     #initialize an array of 6n x 6n zeros (make sure there's no double count)
     for element in system:
         if isinstance(element, Node):
             node_counter += 1
-    matrix = np.zeros((6*node_counter,6*node_counter))
+    matrix = np.zeros((12*node_counter,12*node_counter))
     #if element is a beam then there will be stretch+bend+twist, cross terms -2 moduli
     for element in system:
+        # generate matrix
         if isinstance(element, Beam):
-            # 6 DoF (3 translational 2 bend 1 twist) for beam element corresponding to node 1
-            matrix[6*element.node_index[0], 6*element.node_index[0]] = element.stretch_mod
-            matrix[6*element.node_index[0]+1, 6*element.node_index[0]+1] = element.stretch_mod
-            matrix[6*element.node_index[0]+2, 6*element.node_index[0]+2] = element.stretch_mod
-            matrix[6*element.node_index[0]+3, 6*element.node_index[0]+3] = element.bend_mod
-            matrix[6*element.node_index[0]+4, 6*element.node_index[0]+4] = element.bend_mod
-            matrix[6*element.node_index[0]+5, 6*element.node_index[0]+5] = element.twist_mod
+        # initialise variables
+            # e1 vector node 1
+            e1_x1 = element.N1_e1_u[0] 
+            e1_y1 = element.N1_e1_u[1]
+            e1_z1 = element.N1_e1_u[2]
+            # e1 vector node 2    
+            e1_x2 = element.N2_e1_u[0] 
+            e1_y2 = element.N2_e1_u[1]
+            e1_z2 = element.N2_e1_u[2]
+            # e2 vector node 1 
+            e2_x1 = element.N1_e2_u[0] 
+            e2_y1 = element.N1_e2_u[1]
+            e2_z1 = element.N1_e2_u[2]
+            # e2 vector node 2
+            e2_x2 = element.N2_e2_u[0] 
+            e2_y2 = element.N2_e2_u[1]
+            e2_z2 = element.N2_e2_u[2]
+            # e3 vector node 1
+            e3_x1 = element.N1_e3_u[0] 
+            e3_y1 = element.N1_e3_u[1]
+            e3_z1 = element.N1_e3_u[2]
+            # e3 vector node 2
+            e3_x2 = element.N2_e3_u[0] 
+            e3_y2 = element.N2_e3_u[1]
+            e3_z2 = element.N2_e3_u[2]
+            #Useful terms
+            e1e1_sum = e1_x1*e1_x2 + e1_y1*e1_y2 + e1_z1*e1_z2
+            e2e2_sum = e2_x1*e2_x2 + e2_y1*e2_y2 + e2_z1*e2_z2
+            e3e3_sum = e3_x1*e3_x2 + e3_y1*e3_y2 + e3_z1*e3_z2
+        
+            denom_e1e1_1 = np.sqrt(1-e1e1_sum**2)
+            denom_e1e1_2 = 2*(1-(e1e1_sum)**2)**(3/2)
+            denom_e1e1_3 = 1-(e1e1_sum)**2
+        
+            denom_e2e2_1 = np.sqrt(1-e2e2_sum**2)
+            denom_e2e2_2 = 2*(1-(e2e2_sum)**2)**(3/2)
+            denom_e2e2_3 = 1-(e2e2_sum)**2
+        
+            denom_e3e3_1 = np.sqrt(1-e3e3_sum**2)
+            denom_e3e3_2 = 2*(1-(e3e3_sum)**2)**(3/2)
+            denom_e3e3_3 = 1-(e3e3_sum)**2
+        
+     #Calculate derivatives noting that modulus of unit vectors is 1
+        #e1-e1 vector derivatives
+            dx1dx1_e1 = (((np.arccos(e1e1_sum))*(element.bend_mod)*(-2*e1_x2*e1e1_sum + 2*e1_x1*(e1e1_sum)**2)*(e1_x2-e1_x1*e1e1_sum))/denom_e1e1_2
+            + ((element.bend_mod) * (e1_x2-e1_x1*e1e1_sum)**2)/denom_e1e1_3
+            - ((np.arccos(e1e1_sum)) * (element.bend_mod) * (-2*e1_x1*e1_x2 + 3*(e1_x1**2)*e1e1_sum - e1e1_sum)) / denom_e1e1_1)
+            
+            dx1dx2_e1 = (((np.arccos(e1e1_sum))*(element.bend_mod)*(-2*e1_x1*e1e1_sum + 2*e1_x2*(e1e1_sum)**2)*(e1_x2-e1_x1*e1e1_sum))/denom_e1e1_2
+            + ((element.bend_mod) * (e1_x1-e1_x2*e1e1_sum) * (e1_x2 - e1_x1*e1e1_sum))/denom_e1e1_3
+            - ((np.arccos(e1e1_sum)) * (element.bend_mod) * (e1_x1*e1_x2*e1e1_sum + 1 - e1_x2**2 - e1_x1**2)) / denom_e1e1_1)
+            
+            dx2dx2_e1 = ((((np.arccos(e1e1_sum))*(element.bend_mod)*(-2*e1_x1*e1e1_sum + 2*e1_x2*(e1e1_sum)**2)*(e1_x1-e1_x2*e1e1_sum))/denom_e1e1_2
+            + ((element.bend_mod) * (e1_x1-e1_x2*e1e1_sum)**2)/denom_e1e1_3
+            - ((np.arccos(e1e1_sum)) * (element.bend_mod) * (-2*e1_x1*e1_x2 + 3*(e1_x2**2)*e1e1_sum - e1e1_sum)) / denom_e1e1_1))
+            
+            dx1dy1_e1 = (((np.arccos(e1e1_sum))*(element.bend_mod)*(-2*e1_y2*e1e1_sum + 2*e1_y1*(e1e1_sum)**2)*(e1_x2-e1_x1*e1e1_sum))/denom_e1e1_2
+            + ((element.bend_mod) * (e1_x2-e1_x1*e1e1_sum) * (e1_y2-e1_y1*(e1e1_sum)))/denom_e1e1_3
+            - ((np.arccos(e1e1_sum)) * (element.bend_mod) * (3*e1_x1*e2_y1*e1e1_sum - e1_x1*e1_y2 - e1_x2*e1_y1) / denom_e1e1_1))
+            
+            dx2dy1_e1 = (((np.arccos(e1e1_sum))*(element.bend_mod)*(-2*e1_y2*e1e1_sum + 2*e1_y1*(e1e1_sum)**2)*(e1_x1-e1_x2*e1e1_sum))/denom_e1e1_2
+            + ((element.bend_mod) * (e1_x1-e1_x2*e1e1_sum) * (e1_y2-e1_y1*(e1e1_sum)))/denom_e1e1_3
+            - ((np.arccos(e1e1_sum)) * (element.bend_mod) * (e1_x2*e1_y1*e1e1_sum - e1_x2*e1_y2 - e1_x1*e1_y1) / denom_e1e1_1))
+ 
+            dx1dy2_e1 = (((np.arccos(e1e1_sum))*(element.bend_mod)*(2*e1_y2*e1e1_sum**2 + -2*e1_y1*(e1e1_sum))*(e1_x2-e1_x1*e1e1_sum))/denom_e1e1_2
+            + ((element.bend_mod) * (e1_x2-e1_x1*e1e1_sum) * (e1_y1-e1_y2*(e1e1_sum)))/denom_e1e1_3
+            - ((np.arccos(e1e1_sum)) * (element.bend_mod) * (e1_x1*e1_y2*e1e1_sum - e1_x2*e1_y2 - e1_x1*e1_y1) / denom_e1e1_1))
+            
+            dx2dy2_e1 = (((np.arccos(e1e1_sum))*(element.bend_mod)*(2*e1_y2*e1e1_sum**2 + -2*e1_y1*(e1e1_sum))*(e1_x1-e1_x2*e1e1_sum))/denom_e1e1_2
+            + ((element.bend_mod) * (e1_x1-e1_x2*e1e1_sum) * (e1_y1-e1_y2*(e1e1_sum)))/denom_e1e1_3
+            - ((np.arccos(e1e1_sum)) * (element.bend_mod) * (3*e1_x2*e1_y2*e1e1_sum - e1_x2*e1_y2 - e1_x1*e1_y1) / denom_e1e1_1))
+ 
+            dx1dz1_e1 = (((np.arccos(e1e1_sum))*(element.bend_mod)*(2*e1_z1*e1e1_sum**2 + -2*e1_z2*(e1e1_sum))*(e1_x2-e1_x1*e1e1_sum))/denom_e1e1_2
+            + ((element.bend_mod) * (e1_x2-e1_x1*e1e1_sum) * (e1_z2-e1_z1*(e1e1_sum)))/denom_e1e1_3
+            - ((np.arccos(e1e1_sum)) * (element.bend_mod) * (3*e1_x1*e1_z1*e1e1_sum - e1_x1*e1_z2 - e1_x2*e1_z1) / denom_e1e1_1))
+ 
+            dx2dz1_e1 = (((np.arccos(e1e1_sum))*(element.bend_mod)*(2*e1_z1*e1e1_sum**2 + -2*e1_z2*(e1e1_sum))*(e1_x1-e1_x2*e1e1_sum))/denom_e1e1_2
+            + ((element.bend_mod) * (e1_x1-e1_x2*e1e1_sum) * (e1_z2-e1_z1*(e1e1_sum)))/denom_e1e1_3
+            - ((np.arccos(e1e1_sum)) * (element.bend_mod) * (e1_x2*e1_z1*e1e1_sum - e1_x1*e1_z1 - e1_x2*e1_z2) / denom_e1e1_1))
+ 
+            dx1dz2_e1 = (((np.arccos(e1e1_sum))*(element.bend_mod)*(2*e1_z2*e1e1_sum**2 + -2*e1_z1*(e1e1_sum))*(e1_x2-e1_x1*e1e1_sum))/denom_e1e1_2
+            + ((element.bend_mod) * (e1_z1-e1_z2*e1e1_sum) * (e1_x2-e1_x1*(e1e1_sum)))/denom_e1e1_3
+            - ((np.arccos(e1e1_sum)) * (element.bend_mod) * (e1_x1*e1_z2*e1e1_sum - e1_x1*e1_z1 - e1_x2*e1_z2) / denom_e1e1_1))
+ 
+            dx2dz2_e1 = (((np.arccos(e1e1_sum))*(element.bend_mod)*(2*e1_z2*e1e1_sum**2 + -2*e1_z1*(e1e1_sum))*(e1_x1-e1_x2*e1e1_sum))/denom_e1e1_2
+            + ((element.bend_mod) * (e1_z1-e1_z2*e1e1_sum) * (e1_x1-e1_x2*(e1e1_sum)))/denom_e1e1_3
+            - ((np.arccos(e1e1_sum)) * (element.bend_mod) * (3*e1_x2*e1_z2*e1e1_sum - e1_x2*e1_z1 - e1_x1*e1_z2) / denom_e1e1_1))
+ 
+            dy1dy1_e1 = (((np.arccos(e1e1_sum))*(element.bend_mod)*(2*e1_y1*e1e1_sum**2 + -2*e1_y2*(e1e1_sum))*(e1_y2-e1_y1*e1e1_sum))/denom_e1e1_2
+            + ((element.bend_mod) * (e1_y2-e1_y1*e1e1_sum)**2)/denom_e1e1_3
+            - ((np.arccos(e1e1_sum)) * (element.bend_mod) * (-2*e1_y1*e1_y2 + 3*(e1_y1**2)*e1e1_sum - e1e1_sum)) / denom_e1e1_1)
+ 
+            dy1dy2_e1 = (((np.arccos(e1e1_sum))*(element.bend_mod)*(2*e1_y2*e1e1_sum**2 + -2*e1_y1*(e1e1_sum))*(e1_y2-e1_y1*e1e1_sum))/denom_e1e1_2
+            + ((element.bend_mod) * (e1_y1-e1_y2*e1e1_sum) * (e1_y2-e1_y1*(e1e1_sum)))/denom_e1e1_3
+            - ((np.arccos(e1e1_sum)) * (element.bend_mod) * (e1_y1*e1_y2*e1e1_sum - e1_y1**2 - e1_y2**2 + 1) / denom_e1e1_1))
+ 
+            dy2dy2_e1 = (((np.arccos(e1e1_sum))*(element.bend_mod)*(2*e1_y2*e1e1_sum**2 + -2*e1_y1*(e1e1_sum))*(e1_y1-e1_y2*e1e1_sum))/denom_e1e1_2
+            + ((element.bend_mod) * (e1_y1-e1_y2*e1e1_sum)**2)/denom_e1e1_3
+            - ((np.arccos(e1e1_sum)) * (element.bend_mod) * (-2*e1_y1*e1_y2 + 3*(e1_y2**2)*e1e1_sum - e1e1_sum)) / denom_e1e1_1)
+ 
+            dy1dz1_e1 = (((np.arccos(e1e1_sum))*(element.bend_mod)*(2*e1_z1*e1e1_sum**2 + -2*e1_z2*(e1e1_sum))*(e1_y2-e1_y1*e1e1_sum))/denom_e1e1_2
+            + ((element.bend_mod) * (e1_y2-e1_y1*e1e1_sum) * (e1_z2-e1_z1*(e1e1_sum)))/denom_e1e1_3
+            - ((np.arccos(e1e1_sum)) * (element.bend_mod) * (3*e1_y1*e1_z1*e1e1_sum - e1_y2*e1_z1 - e1_y1*e1_z2) / denom_e1e1_1))
+ 
+            dy2dz1_e1 = (((np.arccos(e1e1_sum))*(element.bend_mod)*(2*e1_z1*e1e1_sum**2 + -2*e1_z2*(e1e1_sum))*(e1_y1-e1_y2*e1e1_sum))/denom_e1e1_2
+            + ((element.bend_mod) * (e1_y1-e1_y2*e1e1_sum) * (e1_z2-e1_z1*(e1e1_sum)))/denom_e1e1_3
+            - ((np.arccos(e1e1_sum)) * (element.bend_mod) * (e1_y2*e1_z1*e1e1_sum - e1_y2*e1_z2 - e1_y1*e1_z1) / denom_e1e1_1))
+ 
+            dy1dz2_e1 = (((np.arccos(e1e1_sum))*(element.bend_mod)*(2*e1_z2*e1e1_sum**2 + -2*e1_z1*(e1e1_sum))*(e1_y2-e1_y1*e1e1_sum))/denom_e1e1_2
+            + ((element.bend_mod) * (e1_y2-e1_y1*e1e1_sum) * (e1_z1-e1_z2*(e1e1_sum)))/denom_e1e1_3
+            - ((np.arccos(e1e1_sum)) * (element.bend_mod) * (e1_y1*e1_z2*e1e1_sum - e1_y2*e1_z2 - e1_y1*e1_z1) / denom_e1e1_1))
+            
+ 
+     #12 DoF (3 translational 2 bend 1 twist) for beam element corresponding to node 1
+            matrix[12*element.node_index[0], 12*element.node_index[0]] = element.stretch_mod
+            matrix[12*element.node_index[0]+1, 12*element.node_index[0]+1] = element.stretch_mod
+            matrix[12*element.node_index[0]+2, 12*element.node_index[0]+2] = element.stretch_mod
             # 6 DoF (3 translational 2 bend 1 twist) for beam element corresponding to node 2
-            matrix[6*element.node_index[1], 6*element.node_index[1]] = element.stretch_mod
-            matrix[6*element.node_index[1]+1, 6*element.node_index[1]+1] = element.stretch_mod
-            matrix[6*element.node_index[1]+2, 6*element.node_index[1]+2] = element.stretch_mod
-            matrix[6*element.node_index[1]+3, 6*element.node_index[1]+3] = element.bend_mod
-            matrix[6*element.node_index[1]+4, 6*element.node_index[1]+4] = element.bend_mod
-            matrix[6*element.node_index[1]+5, 6*element.node_index[1]+5] = element.twist_mod
+            matrix[12*element.node_index[1], 12*element.node_index[1]] = element.stretch_mod
+            matrix[12*element.node_index[1]+1, 12*element.node_index[1]+1] = element.stretch_mod
+            matrix[12*element.node_index[1]+2, 12*element.node_index[1]+2] = element.stretch_mod
             # Cross terms for beam element
-            matrix[6*element.node_index[0], 6*element.node_index[1]] = -2*element.stretch_mod
-            matrix[6*element.node_index[0]+1, 6*element.node_index[1]+1] = -2*element.stretch_mod
-            matrix[6*element.node_index[0], 6*element.node_index[1]] = -2*element.stretch_mod
-            matrix[6*element.node_index[1], 6*element.node_index[0]] = -2*element.stretch_mod
-            matrix[6*element.node_index[1]+1, 6*element.node_index[0]+1] = -2*element.stretch_mod
-            matrix[6*element.node_index[1]+1, 6*element.node_index[0]+1] = -2*element.stretch_mod
+            matrix[12*element.node_index[0], 12*element.node_index[1]] = element.stretch_mod
+            matrix[12*element.node_index[0]+1, 12*element.node_index[1]+1] = element.stretch_mod
+            matrix[12*element.node_index[0], 12*element.node_index[1]] = element.stretch_mod
             
     #if element is a torsional spring then there are 3 DoFs for rotations in x, y, z
         elif isinstance(element, Spring):
             # Node 1
-            matrix[6*element.node_index[0]+3, 6*element.node_index[0]+3] = element.rot_x
-            matrix[6*element.node_index[0]+4, 6*element.node_index[0]+4] = element.rot_y
-            matrix[6*element.node_index[0]+5, 6*element.node_index[0]+5] = element.rot_z
+            matrix[12*element.node_index[0]+3, 12*element.node_index[0]+3] = element.rot_x
+            matrix[12*element.node_index[0]+4, 12*element.node_index[0]+4] = element.rot_y
+            matrix[12*element.node_index[0]+5, 12*element.node_index[0]+5] = element.rot_z
             # Node 2
-            matrix[6*element.node_index[1]+3, 6*element.node_index[1]+3] = element.rot_x
-            matrix[6*element.node_index[1]+4, 6*element.node_index[1]+4] = element.rot_y
-            matrix[6*element.node_index[1]+5, 6*element.node_index[1]+5] = element.rot_z
-    '''if element is a nick there will be reduced stretch, bend and twist moduli
-    single or double crossovers, open nicks and bulges modeled as torsional springs'''
-    print(pd.DataFrame(matrix))
+            matrix[12*element.node_index[1]+3, 12*element.node_index[1]+3] = element.rot_x
+            matrix[12*element.node_index[1]+4, 12*element.node_index[1]+4] = element.rot_y
+            matrix[12*element.node_index[1]+5, 12*element.node_index[1]+5] = element.rot_z
+    #if element is a nick there will be reduced stretch, bend and twist moduli
+    # single or double crossovers, open nicks and bulges modeled as torsional springs
+    
+    print(dx1dx1_e1, 'dx1dx1_e1 test')
+    #print(pd.DataFrame(matrix))
     return matrix
+ 
 test_system = [test_node_1, test_node_2, test_beam_1] 
-generate_matrix(system)
-
-
+ 
+ 
+ 
 def visualise(matrix: np.ndarray, show: bool = False):
     """Taken from here: https://matplotlib.org/stable/gallery/images_contours_and_fields/image_annotated_heatmap.html
     """
@@ -236,17 +353,15 @@ def visualise(matrix: np.ndarray, show: bool = False):
         ax.plot([pos, pos], [-0.5, len(matrix)-0.5], 'k-')
     if show:
         plt.show()
-
-       
+ 
+generate_matrix(test_system)  
 def main():
     matrix = generate_matrix(system)
     #visualise_spring_system(system)
     visualise(matrix, show=True)
     return
-
+ 
 if __name__ == '__main__':
-    main()
-
-
-
+    pass
+    #main()
 
