090c6b6375b09b75e5c85a15a6ff65b408927d6b
Christopher Lau <lauchristopher98@gmail.com>
2021-03-09 11:48:03 +0800

    Added energy calculation function called energy_function

--- sandbox/Spring_system.py
+++ sandbox/Spring_system.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import itertools
-
+from typing import Tuple
 import pandas as pd
 '''
 The Node class creates a node object that has a position accessed by self.get_position. The set_position method allows the user
@@ -12,10 +12,11 @@ vector product of e1 and e2
 '''
 
 class Node:
-    id_iter = itertools.count()
+    id_iter = itertools.count(start=0, step=1)
     def __init__(self):
         self.position = np.array([[0,0,0], [0,0,0], [0,0,0], [0,0,0]])
-        self.index = next(Node.id_iter)
+        self.index = next(self.id_iter)-2
+        self.connectors = []
     def set_position(self, e0, e1, e2, e3):
         '''
         Parameters: 
@@ -30,71 +31,69 @@ class Node:
     def get_position(self):
         return self.position
 
-# test_node = Node()
-# test_node.set_position([1,1,1], [0, -1, -1], [0, -2, -1])
-# print(type(test_node.position)) # returns ndarray
-# print(test_node.position)
-
 class Connector:
     def __init__(self, node_1, node_2):
         assert isinstance(node_1, Node)
         assert isinstance(node_2, Node)
-        self.nodes = node_1, node_2
-        self.node_index = [node_1.index, node_2.index]
-        #Calculates unit vectors of node 1 for e1, e2 and e3 vectors
-        self.N1_e1_u = node_1.position[1] / np.linalg.norm(node_1.position[1])
-        self.N1_e2_u = node_1.position[2] / np.linalg.norm(node_1.position[2])
-        self.N1_e3_u = node_1.position[3] / np.linalg.norm(node_1.position[3])
-        #Calculates unit vectors of node 2 for e1, e2 and e3 vectors
-        self.N2_e1_u = node_2.position[1] / np.linalg.norm(node_2.position[1])
-        self.N2_e2_u = node_2.position[2] / np.linalg.norm(node_2.position[2])
-        self.N2_e3_u = node_2.position[3] / np.linalg.norm(node_2.position[3]) 
+        self.node_1 = node_1
+        self.node_2 = node_2
+        for node in self.node_1, self.node_2:
+            if self not in node.connectors:
+                node.connectors.append(self)
 
-    def connect_nodes(self, node_1, node_2):
-        self.starting_pos = node_1.position[0]
-        self.ending_pos = node_2.position[0]
-        return self
-    def find_angle(self, node_1, node_2):
-        def unit_vector(vector):
-            return vector / np.linalg.norm(vector)
-        v1_u = unit_vector(node_1.position[2])
-        v2_u = unit_vector(node_2.position[2])
-        self.twist_angle = np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
-        v3_u = unit_vector(node_1.position[1])
-        v4_u = unit_vector(node_2.position[1])
-        self.bend_angle_1 = np.arccos(np.clip(np.dot(v3_u, v4_u), -1.0, 1.0))
-        v5_u = unit_vector(node_1.position[3])
-        v6_u = unit_vector(node_2.position[3])
-        self.bend_angle_2 = np.arccos(np.clip(np.dot(v5_u, v6_u), -1.0, 1.0))
-        return self
-
+    @property
+    def node_index(self) -> Tuple[int]:
+        """Indices of nodes, with node_1 first and node_2 second
+        """
+        return (self.node_1.index, self.node_2.index)
+    @property
+    def vector(self) -> np.ndarray:
+        """Absolute vector from node_1 to node_2
+        """
+        return self.node_2.position[0] - self.node_1.position[0]
+    @property
+    def length(self) -> float:
+        """Distance between a connector's two nodes
+        """
+        return np.linalg.norm(self.vector)
+    @property
+    def direction(self) -> np.ndarray:
+        """Unit vector of direction from node_1 to node_2
+        """
+        return self.vector / self.length
+     
 class Beam(Connector):
     def __init__(self, *args):
         super().__init__(*args)
-        super().connect_nodes(*args)
-        super().find_angle(*args)        
         self.duplex = True
         self.stretch_mod = 1100 #pN
         self.bend_mod = 230 #pN nm^2
         self.twist_mod = 460 #pN nm^2
-        self.orientation = args[1].position[0] - args[0].position[0]
-        self.length = np.linalg.norm(self.orientation)  
-        
-    def get_orientation(self):
-        return self.orientation
-    def get_length(self):
-        return self.length
-    def get_angles(self):
-        return self.bend_angle_1, self.bend_angle_2, self.twist_angle
+        def find_angle(v1, v2):
+            v1_u = v1 / np.linalg.norm(v1)
+            v2_u = v2 / np.linalg.norm(v2)
+            return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+        self.twist_angle = find_angle(self.node_1.position[2], self.node_2.position[2])
+
+
     def nick_beam(self):
         self.duplex = False
         self.bend_mod = self.bend_mod/100
         self.twist_mod = self.twist_mod/100
+
+if __name__ == "__main__":
+    nodes = [
+        Node(),
+        Node(),
+    ]
+    connector = Connector(nodes[0], nodes[1])
+    for i, node in enumerate(nodes):
+        print(f"Node {i} Connectors: {node.connectors}")
+        assert connector in node.connectors   
         
 class Spring(Connector):
     def __init__(self, *args, bulge=False):
         super().__init__(*args)
-        self.angle = super().find_angle(*args)
         
         self.rot_x = 1353 #pN nm rad-1
         self.rot_y = 1353 #pN nm rad-1
@@ -123,6 +122,7 @@ class Spring(Connector):
             
 
 def visualise_spring_system(system):    
+    pass
     node_pos = []
     x_pos_node = []
     y_pos_node = []
@@ -137,8 +137,7 @@ def visualise_spring_system(system):
             y_pos_node.append(element.position[0][1])
             z_pos_node.append(element.position[0][2])
         elif isinstance(element, Beam):
-            beam_start_pos.append(element.starting_pos)
-            beam_end_pos.append(element.ending_pos)
+            pass
                                 
     ax = plt.axes(projection = "3d")
     ax.scatter3D(x_pos_node, y_pos_node, z_pos_node, color = "green")
@@ -146,18 +145,27 @@ def visualise_spring_system(system):
     plt.show()
 
 def energy_function(system):
-    nodes = []
-    beams = []
-    springs = []
+    energy = 0
+    L0 = 0.34 #nm equilibrium bond distance
+    eq_theta_twist = 0.5985 # 34.29 degrees
+    def find_angle(v1, v2):
+        v1_u = v1 / np.linalg.norm(v1)
+        v2_u = v2 / np.linalg.norm(v2)
+        return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
     for element in system:
-        if isinstance(element, Node):
-            nodes.append(element)
-        elif isinstance(element, Beam):
-            beams.append(element)
-        elif isinstance(element, Spring):
-            springs.append(element)
-    print(nodes, "\n", beams, "\n", springs)
-    
+        if isinstance(element, Beam):
+            stretch_energy_x = (1/2) * element.stretch_mod * (np.absolute(element.node_1.position[0][0] - element.node_2.position[0][0])-L0) ** 2
+            stretch_energy_y = (1/2) * element.stretch_mod * (np.absolute(element.node_1.position[0][1] - element.node_2.position[0][1])-L0) ** 2
+            stretch_energy_z = (1/2) * element.stretch_mod * (np.absolute(element.node_1.position[0][0] - element.node_2.position[0][0])-L0) ** 2
+            twist_energy = (1/2) * element.twist_mod * (element.twist_angle - eq_theta_twist) ** 2
+            energy += stretch_energy_x + stretch_energy_y + stretch_energy_z + twist_energy
+        elif isinstance(element, Node):
+            if len(element.connectors) == 2:
+                bend_angle =  find_angle(element.connectors[0].direction, element.connectors[1].direction)
+                bend_energy = (1/2) * element.connectors[0].bend_mod * (bend_angle - 0) **2
+                energy += bend_energy
+                
+    return energy
 def generate_matrix(system):  
     #print(pd.DataFrame(matrix))        
     return -1
@@ -182,25 +190,35 @@ def visualise(matrix: np.ndarray, show: bool = False):
 
 test_node_1 = Node()
 test_node_1.set_position([0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0])
+
 test_node_2 = Node()
 test_node_2.set_position([1, 0, 0], [1, 0, 0], [0, 0, 1], [0, 1, 0])
+
 test_node_3 = Node()
 test_node_3.set_position([1, 1, 0], [2, 1, 1], [1,-1,-2], [1, 1, 1])
+
 test_node_4 = Node()
 test_node_4.set_position([0, 1, 0], [1, 1, 1], [0, 2, 2], [1, 1, 1])
+
+# Need to assert that only the elements used are initialised
 test_beam_1 = Beam(test_node_1, test_node_2)
 test_beam_2 = Beam(test_node_2, test_node_3)
+
+'''
 test_beam_3 = Beam(test_node_3, test_node_4)
 test_beam_4 = Beam(test_node_4, test_node_1)
-square_system = [test_node_1, test_node_2, test_node_3, test_beam_4,
-          test_beam_1, test_beam_2, test_beam_3, test_node_4
-          ]
-test_spring = Spring(test_node_1, test_node_2)
-test_system = [test_node_1, test_node_2, test_beam_1] 
 
-energy_function(square_system)  
-def main():
-    matrix = generate_matrix(square_system)
+
+
+square_system = [test_node_1, test_node_2, test_node_3, test_node_4, 
+          test_beam_1, test_beam_2, test_beam_3, test_beam_4,
+          ]
+
+'''
+test_system = [test_node_1, test_node_2, test_beam_1] 
+print("Energy = ", energy_function(test_system))
+
+def main():    
     #visualise_spring_system(system)
     visualise(matrix, show=True)
     return
