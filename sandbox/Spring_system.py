89c11989054de778447d5162fd259229a6ce9936
Christopher Lau <lauchristopher98@gmail.com>
2021-03-17 14:34:22 +0800

    Implemented mrDNA model for bond energies calculation not including xovers

--- sandbox/Spring_system.py
+++ sandbox/Spring_system.py
@@ -5,31 +5,29 @@ import itertools
 from typing import Tuple
 import pandas as pd
 '''
-The Node class creates a node object that has a position accessed by self.get_position. The set_position method allows the user
-to input a numpy array with elements 0,1,2,3 representing the e0, e1, e2, and e3 vectors, respectively. e0 is the position vector of the node, e1
-points towards the major groove, e2 points towards the base in a chosen strand and e3 is the 
-vector product of e1 and e2
+Node class creates a node with a position and a index
 '''
 
 class Node:
-    id_iter = itertools.count(start=0, step=1)
+    id_iter = itertools.count(start=-2, step=1) #not counting properly
     def __init__(self):
-        self.position = np.array([[0,0,0], [0,0,0], [0,0,0], [0,0,0]])
-        self.index = next(self.id_iter)-2
+        self.position = np.array([0,0,0])
+        self.index = next(self.id_iter)
         self.connectors = []
-    def set_position(self, e0, e1, e2, e3):
-        '''
-        Parameters: 
-        Input: e0, e1, e2, e3 (ndarray)  
-        Output: [e0, e1, e2, e3] (ndarray) dim=4x3 
-        '''
-        self.position[0] = np.asarray(e0)
-        self.position[1] = np.asarray(e1)
-        self.position[2] = np.asarray(e2)
-        self.position[3] = np.asarray(e3)
-        
+        self.orientation_node = False
+        self.dsDNA = True
+    
+    def set_position(self, e0):
+        self.position = np.asarray(e0)        
+    
     def get_position(self):
         return self.position
+
+class OrientationNode(Node):
+    def __init__(self, *args):
+        self.connectors = []
+        self.position = np.array([-1, -1, -1])
+        self.orientation_node = True
 
 class Connector:
     def __init__(self, node_1, node_2):
@@ -50,7 +48,7 @@ class Connector:
     def vector(self) -> np.ndarray:
         """Absolute vector from node_1 to node_2
         """
-        return self.node_2.position[0] - self.node_1.position[0]
+        return self.node_2.position - self.node_1.position
     @property
     def length(self) -> float:
         """Distance between a connector's two nodes
@@ -65,21 +63,11 @@ class Connector:
 class Beam(Connector):
     def __init__(self, *args):
         super().__init__(*args)
-        self.duplex = True
-        self.stretch_mod = 1100 #pN
-        self.bend_mod = 230 #pN nm^2
-        self.twist_mod = 460 #pN nm^2
+        
         def find_angle(v1, v2):
             v1_u = v1 / np.linalg.norm(v1)
             v2_u = v2 / np.linalg.norm(v2)
             return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
-        self.twist_angle = find_angle(self.node_1.position[2], self.node_2.position[2])
-
-
-    def nick_beam(self):
-        self.duplex = False
-        self.bend_mod = self.bend_mod/100
-        self.twist_mod = self.twist_mod/100
 
 if __name__ == "__main__":
     nodes = [
@@ -88,61 +76,53 @@ if __name__ == "__main__":
     ]
     connector = Connector(nodes[0], nodes[1])
     for i, node in enumerate(nodes):
-        print(f"Node {i} Connectors: {node.connectors}")
+        #print(f"Node {i} Connectors: {node.connectors}")
         assert connector in node.connectors   
-        
-class Spring(Connector):
-    def __init__(self, *args, bulge=False):
-        super().__init__(*args)
-        
-        self.rot_x = 1353 #pN nm rad-1
-        self.rot_y = 1353 #pN nm rad-1
-        self.rot_z = 135.3 #pN nm rad-1
+                    
+def set_orientation_nodes(system):
+    orientation_elements = []
+    for node in system:
+        if isinstance(node, Node):
+            if node.dsDNA == True:
+                temp_node = OrientationNode()
+                temp_node.set_position(np.array([node.position[0] + 0.15, node.position[1], node.position[2]])) #0.15 in nm
+                temp_beam = Beam(temp_node, node)
+                orientation_elements.append(temp_node)
+                orientation_elements.append(temp_beam)
+    system = system + orientation_elements
+    return system
 
-        self._bulge = False
 
-        self.bulge = bulge
+def calculate_total_energy(system, T): #T = temperature (K)
+    k_B = 1.38065E-23 
+    total_energy = 0
+    def calculate_bond_energy(r1, r2, k, L_0): #pos vectors r1 and r2 of nodes 1 and 2 
+        x1, x2, y1, y2, z1, z2 = r1[0], r2[0], r1[1], r2[1], r1[2], r2[2]
+        bond_energy = (1/2) * k * ((np.sqrt(np.abs(x1-x2)+np.abs(y1-y2)+np.abs(z1-z2)))-L_0)**2
+        return bond_energy
+    for beam in system:
+        if isinstance(beam, Beam):
+            if (beam.node_1.orientation_node == False and beam.node_2.orientation_node == False) and (beam.node_1.dsDNA and beam.node_2.dsDNA): 
+                # both n1 and n2 are backbone nodes and dsDNA
+                L_0 = 0.34 # nm
+                k_dsDNA = 2.94 # J/m^2
+                energy = calculate_bond_energy(beam.node_1.position, beam.node_2.position, k_dsDNA, L_0)
+                total_energy += energy
+            elif beam.node_1.orientation_node and beam.node_2.dsDNA:
+                # Energy of orientation node connected to a real node
+                assert (beam.node_1.orientation_node and beam.node_2.dsDNA), "Only dsDNA have orientation beads"
+                L_0 = 0.15 #nm
+                k_orient = 20.8 #J/m^2
+                energy = calculate_bond_energy(beam.node_1.position, beam.node_2.position, k_orient, L_0)
+                total_energy += energy
+            elif (beam.node_1.orientation_node == False and beam.node_2.orientation_node == False) and (beam.node_1.dsDNA == False and beam.node_2.dsDNA == False):
+                # both nodes are ssDNA 
+                L_0 = 0.64 # nm
+                k_ssDNA = 2.352 # J/m^2
+                energy = calculate_bond_energy(beam.node_1.position, beam.node_2.position, k_ssDNA, L_0)
+                total_energy += energy
+    return total_energy
 
-    @property
-    def bulge(self):
-        return self._bulge
-
-    @bulge.setter
-    def bulge(self, value: bool):
-        assert isinstance(value, bool)
-        self._bulge = value
-        if value:
-            self.rot_x = 13.53 #pN nm rad-1
-            self.rot_y = 0 #pN nm rad-1
-            self.rot_z = 13.53 #pN nm rad-1
-        else:
-            self.rot_x = 1353 #pN nm rad-1
-            self.rot_y = 1353 #pN nm rad-1
-            self.rot_z = 135.3 #pN nm rad-1
-            
-def energy_function(system):
-    energy = 0
-    L0 = 0.34 #nm equilibrium bond distance
-    eq_theta_twist = 0.5985 # 34.29 degrees    
-    for element in system:
-        if isinstance(element, Beam):
-            stretch_energy_x = (1/2) * element.stretch_mod * (np.absolute(element.node_1.position[0][0] - element.node_2.position[0][0])-L0) ** 2
-            stretch_energy_y = (1/2) * element.stretch_mod * (np.absolute(element.node_1.position[0][1] - element.node_2.position[0][1])-L0) ** 2
-            stretch_energy_z = (1/2) * element.stretch_mod * (np.absolute(element.node_1.position[0][0] - element.node_2.position[0][0])-L0) ** 2
-            twist_energy = (1/2) * element.twist_mod * (element.twist_angle - eq_theta_twist) ** 2
-            energy += stretch_energy_x + stretch_energy_y + stretch_energy_z + twist_energy
-        elif isinstance(element, Node):
-            if len(element.connectors) == 2:
-                cos_angle =  (element.connectors[0].direction[0] * element.connectors[1].direction[0] 
-                + element.connectors[0].direction[1] * element.connectors[1].direction[1]
-                + element.connectors[0].direction[2] * element.connectors[1].direction[2])  
-                bend_energy = (1/2) * element.connectors[0].bend_mod * np.arccos(cos_angle) **2
-                energy += bend_energy
-                
-    return energy
-def generate_matrix(system):  
-    #print(pd.DataFrame(matrix))        
-    return -1
    
 def visualise(matrix: np.ndarray, show: bool = False):
     """Taken from here: https://matplotlib.org/stable/gallery/images_contours_and_fields/image_annotated_heatmap.html
@@ -163,34 +143,36 @@ def visualise(matrix: np.ndarray, show: bool = False):
         plt.show()
 
 test_node_1 = Node()
-test_node_1.set_position([0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0])
+test_node_1.set_position([0, 0, 0])
 
 test_node_2 = Node()
-test_node_2.set_position([1, 0, 0], [1, 0, 0], [0, 0, 1], [0, 1, 0])
-
-test_node_3 = Node()
-test_node_3.set_position([1, 1, 0], [2, 1, 1], [1,-1,-2], [1, 1, 1])
-
-test_node_4 = Node()
-test_node_4.set_position([0, 1, 0], [1, 1, 1], [0, 2, 2], [1, 1, 1])
-
-# Need to assert that only the elements used are initialised
-test_beam_1 = Beam(test_node_1, test_node_2)
-test_beam_2 = Beam(test_node_2, test_node_3)
+test_node_2.set_position([1, 0, 0])
 
 '''
+test_node_3 = Node()
+test_node_3.set_position([1, 1, 0])
+
+test_node_4 = Node()
+test_node_4.set_position([0, 1, 0])
+
+'''
+test_beam_1 = Beam(test_node_1, test_node_2)
+'''
+test_beam_2 = Beam(test_node_2, test_node_3)
+
 test_beam_3 = Beam(test_node_3, test_node_4)
 test_beam_4 = Beam(test_node_4, test_node_1)
-
-
 
 square_system = [test_node_1, test_node_2, test_node_3, test_node_4, 
           test_beam_1, test_beam_2, test_beam_3, test_beam_4,
           ]
-
 '''
+
 test_system = [test_node_1, test_node_2, test_beam_1] 
-print("Energy = ", energy_function(test_system))
+test_system = set_orientation_nodes(test_system)
+print(calculate_total_energy(test_system, 298))
+
+        
 
 def main():       
     visualise(matrix, show=True)
